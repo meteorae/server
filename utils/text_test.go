68068e04a96feb377ae5ee61d07c1351e60fa617
MrTimscampi <julien.machiels@protonmail.com>
2022-06-12 18:47:34 +0200

    test: add placeholder tests for utils

---
+++ utils/text_test.go
@@ -0,0 +1,13 @@
+package utils_test
+
+import "testing"
+
+func TestCleanSortTitle(t *testing.T) {
+	t.Parallel()
+	t.Skip("TODO")
+}
+
+func TestRemoveUnwantedCharacters(t *testing.T) {
+	t.Parallel()
+	t.Skip("TODO")
+}
---
+++ utils/user_test.go
@@ -0,0 +1,13 @@
+package utils_test
+
+import "testing"
+
+func TestGetContextWithUser(t *testing.T) {
+	t.Parallel()
+	t.Skip("TODO")
+}
+
+func TestGetUserFromContext(t *testing.T) {
+	t.Parallel()
+	t.Skip("TODO")
+}
