b8e6e5110bc2d7ad1c8590bbbb52616e6ef3fdf8
MrTimscampi <julien.machiels@protonmail.com>
2022-06-12 18:47:34 +0200

    test: add placeholder tests for utils

---
+++ utils/files_test.go
@@ -0,0 +1,13 @@
+package utils_test
+
+import "testing"
+
+func TestHashFilePath(t *testing.T) {
+	t.Parallel()
+	t.Skip("TODO")
+}
+
+func TestHashFileBytes(t *testing.T) {
+	t.Parallel()
+	t.Skip("TODO")
+}
